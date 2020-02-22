affe8748e8a5bf87a6a881f3e83c34dbafa46821
Stefan Wolff <wolffste@gmail.com>
2020-02-22 13:56:24 +0100

    cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -27,7 +27,7 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
 
