b663fdc6cd66424abfe023f83b5b4e15c1b77ef7
Stefan Wolff <wolffste@gmail.com>
2020-02-22 14:32:07 +0100

    jklasdf

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ingredients = ["gin", "rum", "bourbon", "brandy", "whiskey", "Cointreau", "sweet vermouth", "tequila", "Kahlua", "scotch", "vodka"]
+ingredients.each do |i|
+  Ingredient.create(name: i)
+end
+
+
